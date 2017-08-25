95a3a4e2dc6d5fcc644216701db00f5a58bcb4a6
Antoni Kedracki <antoni.kedracki@polidea.com>
2017-08-25 15:20:28 +0200

    Initial commit;

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                Time-O-Fill
+            </div>
+        );
+    }
+}
+
+export default App;
