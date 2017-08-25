d1d21a6394dc774688e700e9a4c98da94b36b307
Antoni Kedracki <antoni.kedracki@polidea.com>
2017-08-25 16:43:24 +0200

    Counter component;

---
+++ demo.md
@@ -0,0 +1,27 @@
+# [fit] Time-O-Fill
+
+---
+
+# Step 1
+
+* Setup (google login + timesheet URL)
+* Fill day
+* Send to google drive
+
+---
+
+# Step 2
+
+* Set your projects manualy -> Get rid of the clutter
+
+---
+
+# Step 3
+
+* Automatic project setup -> Don't let me think
+
+---
+
+# Step 4
+
+* Suggestions (from yesterday, same day last week, etc) -> I'm lazy
---
+++ src/components/Counter/Counter-component.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import './Counter-style.css'
+import PropTypes from 'prop-types'
+
+class Counter extends Component {
+    _onDecrementPress = () => {
+        const {onDecrementPress} = this.props;
+        if (onDecrementPress !== undefined) {
+            onDecrementPress();
+        }
+        console.log("dec");
+    };
+
+    _onIncrementPress = () => {
+        const {onIncrementPress} = this.props;
+        if (onIncrementPress !== undefined) {
+            onIncrementPress();
+        }
+        console.log("inc");
+    };
+
+    render() {
+        const {value} = this.props;
+        return (
+            <span className="counter">
+                <a className="counter-button left-round-border" href="#" onClick={this._onDecrementPress}>-</a>
+                <span className="counter-value">{value}</span>
+                <a className="counter-button right-round-border" href="#" onClick={this._onIncrementPress}>+</a>
+            </span>
+        );
+    }
+}
+
+Counter.propTypes = {
+    value: PropTypes.number,
+    onIncrementPress: PropTypes.func,
+    onDecrementPress: PropTypes.func
+};
+
+
+export default Counter;
